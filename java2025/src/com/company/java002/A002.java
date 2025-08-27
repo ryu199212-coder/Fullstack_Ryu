a49ed2857212f775d09072fcb6ba8f63fed9b2e2
Ryu <ryu199212@gmail.com>
2025-08-27 17:44:39 +0900

    27일 학원에서 최종

---
+++ java2025/src/com/company/java002/A002.java
@@ -0,0 +1,16 @@
+package com.company.java002;
+
+public class A002 {
+    public static void main(String[] args) {
+    	 System.out.println("A");
+    	 System.out.println("AB");
+    	 System.out.println("ABC");
+    	 
+    	 // 한줄로 만드는 방법은?
+    	 System.out.println("A\nAB\nABC"); //줄바꿈 \n
+    	 
+    	 // 포멧형식
+    	 System.out.printf("이름 : %s 나이 : %d  " , "길동"  , 12 ); 
+    }
+
+}
