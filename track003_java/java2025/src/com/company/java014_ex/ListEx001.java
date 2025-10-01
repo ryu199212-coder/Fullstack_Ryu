6e3599978cc3bf3d859923048be1665ef904c896
Ryu <ryu199212@gmail.com>
2025-10-01 18:09:19 +0900

    10월1일

---
+++ track003_java/java2025/src/com/company/java014_ex/ListEx001.java
@@ -0,0 +1,25 @@
+package com.company.java014_ex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListEx001 {
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+	     list.add("red");
+	     list.add("green");
+	     list.add("blue");
+	     
+	    System.out.println(list);
+		
+	}
+}
+/*연습문제1)  Collection  Framework
+패키지명 : com.company.java014_ex
+클래스명 : ListEx001
+다음과 같이 코드를 작성하시오.
+ 1.  ArrayList이용해서 colors 만들기
+ 2. red, green, blue 데이터 추가
+ 3. 출력
+*/
